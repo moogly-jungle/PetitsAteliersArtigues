4043dc2e9a8f658604ecdea27deb925081f8ea3f
moog <ly.olivier@gmail.com>
2017-03-17 21:09:42 +0100

    déroulement du début de la séance

--- Atelier0/Introduction/Introduction.ino
+++ Atelier0/Introduction/Introduction.ino
@@ -1,6 +1,23 @@
 #include <Servo.h>
 
-#define PIN_POTENTIOMETRE 0
+/* --------------
+ *  Introduction
+ * --------------
+ * Introduction rapide à l'Arduino Uno
+ * 1) Commencer par brancher le moteur et envoyer des valeurs constantes
+ *    - le moteur, c'est motivant, ça sort un peu de l'ordinaire, et c'est lié à la robotique
+ *    - il faut introduire le concept d'angle pour les petits
+ * 1' option) faire une boucle avec un délai pour faire bouger le servo "tout seul"
+ *    - c'est l'occasion de parler de variable, et de boucle (ne pas oublier le délai)
+ * 1'' option) monitorer le signal à l'oscilloscope
+ * 2) Brancher le potentiomètre, faire un retour dans le terminal pour voir concretement bouger la valeur
+ * 3) Piloter le servo avec le potentiomètre, on introduit map
+ * 4) Brancher et tester le buzzer avec le bouton poussoir (ce sera infernal sinon...)
+ * 5) Piloter le buzzer en modifiant la frequence avec le potentiomètre
+ * 6) Brancher, tester la led RGB, puis la piloter avec le potentiomètre.
+ */
+
+#define PIN_POTENTIOMETRE 0 /* brancher le potentiomètre sur A0 (Analog In sur la gauche de la carte) */
 int commande;
 
 #define PIN_MOTEUR 4
@@ -10,6 +27,17 @@ int angle;
 void setup() {
   Serial.begin(9600);
   moteur.attach(PIN_MOTEUR);
+
+  /* option : pour montrer le concept de mouvement,
+   * on fait varier le délai pour faire varier la vitesse */
+  for (angle=0; angle<180; angle = angle+1) { /* angle = angle+1 plutot que angle++ pour simplifier la syntaxe */
+    moteur.write(angle); /* on évite les float, pour les élèves de primaire, c'est un peu complexe */
+    delay(20);
+  }
+  for (angle=180; angle>0; angle = angle-1) {
+    moteur.write(angle);
+    delay(20);
+  }
 }
 
 void loop() {
@@ -25,7 +53,7 @@ void loop() {
    * <Attention> le potentiometre prend sa tension de ref sur l'arduino et le servo sur la batterie
    * sinon, le servo perturbe la tension de référence du potentiomètre */
   angle = map(commande, 0, 1023, 0, 180); /* transforme la commande de [0,1023] -> [0,180] */
-  moteur.write(angle); 
+  // moteur.write(angle); 
   Serial.print("angle = "); Serial.print(angle); Serial.print("\n");
 
   /* pilotage de la led */
